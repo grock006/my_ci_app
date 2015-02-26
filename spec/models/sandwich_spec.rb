96b39a54885fb18d8da5c6fdca0ef50f3270eebf
Greg Rock <grock006@gmail.com>
2015-02-26 11:43:30 -0800

    add heroku

--- spec/models/sandwich_spec.rb
+++ spec/models/sandwich_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Sandwich, type: :model do
   it "should be valid" do
   	expect(Sandwich.new()).to be_valid
   end
+
+  it "should respond to name" do
+  	expect(Sandwich.new()).to respond_to(name)
+  end
 end
